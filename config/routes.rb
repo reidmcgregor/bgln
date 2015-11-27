f54f504b441e47de5510120c7b07b1f7799d3fcb
Reid McGregor <reid.mcgregor@gmail.com>
2015-11-26 22:11:30 -0800

    Updated profile and added scoped game updates to it

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'collection', to: 'games#index', as: :collection
   root to: 'games#index'
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
+  
   test "should get show" do
-    get :show
+    get :show, id: users(:reid).profile_name
     assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "should render a 404 on profile not found" do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+
+  test "that variables are assigned on successful profile viewing" do
+  	get :show, id: users(:reid).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:games)
+  end
+
+  test "only shows games in the users collection" do
+    get :show, id: users(:reid).profile_name
+    assigns(:games).each do |game|
+      assert_equal users(:reid), game.user
+    end
   end
 
 end
